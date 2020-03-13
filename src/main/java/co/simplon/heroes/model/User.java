8f90e5b3551acfc19da872d35de38f0abb586181
Cédric Maunier <cedric.maunier.lp4@gmail.com>
2020-03-13 09:40:20 +0100

    suppression de jwt et mise en place de h2

--- src/main/java/co/simplon/heroes/model/User.java
+++ src/main/java/co/simplon/heroes/model/User.java
@@ -1,21 +1,10 @@
 package co.simplon.heroes.model;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,7 +14,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @NoArgsConstructor
-public class User implements UserDetails {
+public class User  {
 	
 	private static final long serialVersionUID = 1L;
 
@@ -38,47 +27,7 @@ public class User implements UserDetails {
 	// Le password pour l'authentification
 	private String motDePasse;
 	
-	@ElementCollection(fetch = FetchType.EAGER)
-	private List<String> roles = Arrays.asList("USER");
-	
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
-		for(String role: roles) {
-			authorities.add(new SimpleGrantedAuthority(role));
-		}
-		return authorities;
-	}
 
-	@Override
-	public String getUsername() {
-		return pseudo;
-	}
-
-	@Override
-	public String getPassword() {
-		return motDePasse;
-	}
-	
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
 
 
 
